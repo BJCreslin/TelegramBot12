81382eae3caa05706a1852e03a88b0908150e6ee
BJCreslin <bjcreslin@gmail.com>
2018-03-10 16:58:38 +0700

    1

--- src/main/java/TB/Myconstant.java
+++ src/main/java/TB/Myconstant.java
@@ -1,14 +1,5 @@
 package TB;
 
 public class Myconstant {
-    final static private String BotToken= "456915279:AAGoXpOfvd_uSa8pY-x0-wLBqM8EeW_jTX4";
-    final static private String BOTNAME="BJCallBote";
 
-    public static String getBOTNAME() {
-        return BOTNAME;
-    }
-
-    public String getBotToken() {
-        return BotToken;
-    }
 }
