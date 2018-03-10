249012a4817d05ffdec143dc1bc309bbe6000071
BJCreslin <bjcreslin@gmail.com>
2018-03-10 22:49:09 +0700

    1

--- src/main/java/TB/MyBot.java
+++ src/main/java/TB/MyBot.java
@@ -1,6 +1,7 @@
 package TB;
 
 import TB.config.ConfigCommand;
+import TB.reactions.Hello;
 import org.telegram.telegrambots.api.methods.send.SendMessage;
 import org.telegram.telegrambots.api.objects.Update;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
@@ -9,23 +10,33 @@ import org.telegram.telegrambots.exceptions.TelegramApiException;
 public class MyBot extends TelegramLongPollingBot {
     //    @Override
     public void onUpdateReceived(Update update) {
+        String messageTextForAnswer = ""; //переменная, в которой будут содержаться ответы
+
         // We check if the update has a message and the message has text
         if (update.hasMessage() && update.getMessage().hasText()) {
             // Set variables
             String message_text = update.getMessage().getText();
+
+
+            // При запуске бота здороваемся
+            if ("/start".equalsIgnoreCase(message_text)) {
+                messageTextForAnswer = Hello.helloMessage();
+            }
+
+
             if (message_text.toLowerCase().equals("salam")) {
-                message_text = "salam popolam";
+                messageTextForAnswer = "salam popolam";
             }
 
             if (message_text.toLowerCase().equals("привет")) {
-                message_text = "salam popolam";
+                messageTextForAnswer = "salam popolam";
             }
 
             long chat_id = update.getMessage().getChatId();
 
             SendMessage message = new SendMessage() // Create a message object object
                     .setChatId(chat_id)
-                    .setText(message_text);
+                    .setText(messageTextForAnswer);
 
             try {
                 execute(message); // Sending our message object to user
