7c91d69fdebdbfbf2df12f38c97e337411fd731a
BJCreslin <bjcreslin@gmail.com>
2018-03-10 22:16:39 +0700

    1

---
+++ src/main/java/TB/config/ConfigCommand.java
@@ -0,0 +1,15 @@
+package TB.config;
+
+public  class ConfigCommand {
+    final static private String BotToken= "456915279:AAGoXpOfvd_uSa8pY-x0-wLBqM8EeW_jTX4";
+    final static private String BOTNAME="BJCallBote";
+
+    public static String getBOTNAME() {
+
+        return BOTNAME;
+    }
+
+    public static String getBotToken() {
+        return BotToken;
+    }
+}
