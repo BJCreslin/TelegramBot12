454f6d29b60fe02c949bcf2b9a067d11dbe8cec4
bjcreslin <bjcreslin@gmail.com>
2018-03-30 10:44:58 +0700

    обнова

--- src/main/java/TB/reactions/DateCommand.java
+++ src/main/java/TB/reactions/DateCommand.java
@@ -3,17 +3,44 @@ package TB.reactions;
 import datetime.Dates;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
 
 public class DateCommand implements Comandable {
+    static private HashMap<String, String> mapMonth;
+
+    static {
+        mapMonth = new HashMap<>();
+        mapMonth.put("january", "Января");
+        mapMonth.put("february", "Февраля");
+        mapMonth.put("march", "Марта");
+        mapMonth.put("april", "Апреля");
+        mapMonth.put("may", "Мая");
+        mapMonth.put("june", "Июня");
+        mapMonth.put("july", "Июля");
+        mapMonth.put("august", "Августа");
+        mapMonth.put("september", "Сентября");
+        mapMonth.put("october", "Октября");
+        mapMonth.put("november", "Ноября");
+        mapMonth.put("december", "Декабря");
+
+
+    }
+
     @Override
     public String execute() {
         Dates dates=new Dates();
 
         LocalDateTime timePoint = dates.getDate();
-        String dateText="Сегодня:";
+        String dateText = "Сегодня: \uD83D\uDCC6 \n";
         dateText += String.valueOf(timePoint.getDayOfMonth());
-        dateText += " " + timePoint.getMonth().name() + " ";
+        dateText += " " + getRusMonthByEngName(timePoint.getMonth().name()) + " ";
         dateText += timePoint.getYear();
         return dateText;
     }
+
+    private String getRusMonthByEngName(String engName) {
+        return mapMonth.get(engName.toLowerCase());
+    }
+
+
 }
